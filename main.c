d868481f4d8a16eebeb7165b8e575e4147a1a69d
LinLai <zzbsn.cn@outlook.com>
2024-12-23 08:13:29 +0800

    w

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello World\n");
+void bubbleSort(int arr[], int n) {
+    int i, j, swap;
+    for (i = 0; i < n-1; i++) {       
+
+
+
+
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = swap;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    
+    printf("\n");
+
+    bubbleSort(arr, n);
+    
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    
     return 0;
 }
