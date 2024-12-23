32b9bde2326504158782d09be61271decac0b077
LinLai <zzbsn.cn@outlook.com>
2024-12-23 09:39:38 +0800

    add macro template

--- lib/bank_db.h
+++ lib/bank_db.h
@@ -8,3 +8,15 @@
  * @copyright Copyright (c) 2024
  * 
  */
+#ifndef BANK_DB_H
+#define BANK_DB_H
+
+#ifdef BANK_DB_EXPORTS
+#define BANK_DB_API __declspec(dllexport)
+#else
+#define BANK_DB_API __declspec(dllimport)
+#endif
+
+//code
+
+#endif // BANK_DB_H
